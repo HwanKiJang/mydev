82b7f08cd2f1c256db8977a3888252be78303662
HwanKi Jang <darkrmeo@gmail.com>
2022-02-25 21:28:57 +0900

    클릭 이벤트 테스트

--- momentum/app.js
+++ momentum/app.js
@@ -3,7 +3,7 @@ const title = document.querySelector("div.hello:first-child h1");
 console.log(title);
 
 function handleTitleClick(){
-    console.log("title was clicked!");
+    title.style.color = "blue";
 }
 
 title.addEventListener("click",handleTitleClick);
